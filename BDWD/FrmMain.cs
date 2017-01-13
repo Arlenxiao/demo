d8eaccafab9666d449df50f0dd79b4d0de091d70
arlenxiao <arlenxiao@live.cn>
2017-01-13 11:30:20 +0800

    百度关键词查询优化,解决关键词查询完毕后崩溃

--- BDWD/FrmMain.cs
+++ BDWD/FrmMain.cs
@@ -26,8 +26,7 @@ namespace BDWD
         {
             index = 0;
             var str = txt_key.Text;
-
-            keys = str.Split(',').ToList();
+            keys = str.Split(new string[] { "", ",", ";", "|" }, StringSplitOptions.RemoveEmptyEntries).ToList();
 
             if (keys.Count <= 0) { MessageBox.Show("请输入关键词"); return; }
 
@@ -36,18 +35,23 @@ namespace BDWD
             Action();
         }
 
+        /// <summary>
+        /// 执行定时动作
+        /// </summary>
         private void Action()
         {
-            if (keys.Count <= 0) return;
-
-            index = index <= keys.Count ? index : 0;
-            key = keys[index];
-            filter = txt_filter.Text;
-            string url = string.Format("http://www.baidu.com/s?wd={0}&pn={1}&usm=2", key, pn * num);
+            //临界判断
+            if (keys.Count <= 0 || index >= keys.Count) return;
 
             this.BeginInvoke((MethodInvoker)(() => { lbl_page.Text = num.ToString(); }));
 
+            index = index < keys.Count ? index : 0;
+            key = keys[index];
+            filter = txt_filter.Text;
+            string url = string.Format("http://www.baidu.com/s?wd={0}&pn={1}&usm=2", key, pn * num);
             GetWebContent(url);
+
+            //百度没有收录情况跳转下一个关键词
             if (num > 75)
             {
                 if (index < keys.Count) { index++; num = 0; }
@@ -55,6 +59,9 @@ namespace BDWD
             }
         }
 
+        /// <summary>
+        /// 进度显示,让人知道程序是活着的
+        /// </summary>
         private void SetPB()
         {
             this.BeginInvoke((MethodInvoker)(() =>
@@ -65,18 +72,30 @@ namespace BDWD
             }));
         }
 
+        /// <summary>关键词集合</summary>
         List<string> keys = new List<string>();
+        /// <summary>当前使用关键词</summary>
         string key = string.Empty;
+        /// <summary>匹配条件</summary>
         string filter = string.Empty;
+        /// <summary>百度查询每页数据</summary>
         int pn = 10;
+        /// <summary>百度查询索引</summary>
         int num = 0;
+        /// <summary>关键词索引</summary>
         int index = 0;
+
+        /// <summary>
+        /// 百度查询界面内容
+        /// </summary>
+        /// <param name="Url"></param>
         private void GetWebContent(string Url)
         {
             SetPB();
             string strResult = "";
             try
             {
+                //网页请求
                 HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
                 HttpWebResponse response = (HttpWebResponse)request.GetResponse();
                 using (Stream streamReceive = response.GetResponseStream())
@@ -85,19 +104,18 @@ namespace BDWD
                     strResult = streamReader.ReadToEnd();
                 }
 
+                //过滤页面脚本
                 var str = Regex.Replace(strResult, @"<script[\s\S]*?</script\s*>", "", RegexOptions.None);
                 var html = str.Replace("\r", "").Replace("\n", "").Replace("\t", "");
                 this.BeginInvoke((MethodInvoker)(() =>
                 {
-                    try
-                    {
-                        webBrowser1.DocumentText = html;
-                    }
-                    catch { }
+                    try { webBrowser1.DocumentText = html; } catch { }
                 }));
 
+                //百度查询结果处理
                 List<Keyword> keywords = new BaiduSearch().GetKeywords(html, key);
 
+                //查询结果匹配
                 if (html.Contains(filter))
                 {
                     index++;
@@ -127,34 +145,47 @@ namespace BDWD
             }
         }
 
-
-
+        /// <summary>
+        /// 定时事件
+        /// </summary>
         private void timer1_Tick(object sender, EventArgs e)
         {
             Action();
         }
 
+        /// <summary>
+        /// 停止方法
+        /// </summary>
         private void btn_stop_Click(object sender, EventArgs e)
         {
             EndSearch();
         }
 
+        /// <summary>
+        /// 方法结束
+        /// </summary>
         private void EndSearch()
         {
             timer1.Enabled = false;
             num = 0;
         }
 
+        /// <summary>
+        /// 下一个关键词
+        /// </summary>
         private void btn_next_Click(object sender, EventArgs e)
         {
             index++;
             timer1.Enabled = true;
             Action();
-            //webBrowser2.Navigate("http://www.wslink.cn/h-index.html");
         }
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
         private void Form1_Load(object sender, EventArgs e)
         {
+            //页面脚本设置
             webBrowser1.ScriptErrorsSuppressed = true;
             webBrowser2.ScriptErrorsSuppressed = true;
         }
